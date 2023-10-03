eced42dfb54d5ea6432714d748ea3955ac91a577
Soawnjaja <basstraper@gmail.com>
2023-10-03 11:45:23 +0300

    test change to use assert from node

--- capitalize/test.capitalize.js
+++ capitalize/test.capitalize.js
@@ -1,7 +1,7 @@
 import capitalize from "./capitalize.js"
 import { strict as assert } from 'node:assert';
 
-assert.equal(capitalize('text'),'Text')
-assert.equal(capitalize(''), '')
+assert.equal(capitalize('text'),'Text');
+assert.equal(capitalize(''), '');
 
-console.log('Все тесты прошли успешно!')
+console.log('Все тесты прошли успешно!');
